bf6cb811b439560307a4b6f53599b4c384833e12
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2022-01-22 12:31:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['https://chat.whatsapp.com/LJHLvvu63DMG9MNBqua30M', 'https://chat.whatsapp.com/EXYsgUhiKy38TguvDcLyko']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
   '6285237596750': { // put your number here
